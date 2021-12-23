60aec87da996d729b3c663dab385c5079e5cfbd7
birgitmelissa <94128573+birgitmelissa@users.noreply.github.com>
2021-12-23 20:19:37 +0100

    Add files via upload

---
+++ opdracht22/script.js
@@ -0,0 +1,14 @@
+const verf = function(soort) {
+    console.log ("verf de muur rood")
+    console.log("de muur is nu " + soort);
+}
+
+const kleur = "groen";
+
+verf(kleur);
+
+const verven = function(noordkleur, zuidkleur) {
+    console.log("de noordmuur is nu " + noordkleur + " en de zuidmuur is nu " + zuidkleur);
+}
+
+verven("grijs", "oranje");
